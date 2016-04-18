926ac1b5fa7771c73cede96d8d78cdb7577209a7
Hou <hou@houdemac-mini.local>
2016-04-18 10:49:26 +0800

    基本功能

---
+++ tngouHealth/tngouHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tngouHealth
+//
+//  Created by hou on 16/4/18.
+//  Copyright © 2016年 houshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
