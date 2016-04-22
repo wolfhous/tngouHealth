d33e53293a2dcefc8a93456e3d86cd70578414c4
Hou <hou@houdemac-mini.local>
2016-04-22 09:54:23 +0800

    地图定位基本实现

--- tngouHealth/tngouHealth/lib/umeng_community_ios_micro_sdk_v2.4/Source/UI/BaseVC/UMComNavigationController.m
+++ tngouHealth/tngouHealth/lib/umeng_community_ios_micro_sdk_v2.4/Source/UI/BaseVC/UMComNavigationController.m
@@ -35,7 +35,18 @@
     }
     // Do any additional setup after loading the view from its nib.
 }
-
+//重写父类的pushViewController方法
+//退出时隐藏底部bar
+-(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated//viewController这个就是要退出的视图
+{
+    if (self.childViewControllers.count > 0) { // 如果push进来的不是第一个控制器
+        viewController.hidesBottomBarWhenPushed = YES;
+        //iOS7及以后的版本支持，self.view.frame.origin.y会下移64像素至navigationBar下方。
+//        viewController.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+    // 这句super的push要放在后面, 让viewController可以覆盖上面设置的leftBarButtonItem
+    [super pushViewController:viewController animated:animated];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
